621c278f84129626c9705d5f22e9c3f58e9a24e6
Santi Mera <santigomero@gmail.com>
2024-09-07 15:41:02 +0300

    added footer and rearranged the components

--- src/components/Products.jsx
+++
@@ -1,7 +0,0 @@
-import classes from "../styles/products.module.css"
-
-const Producs = () => {
-    return (<>Products</>);
-}
-
-export default Producs;
--- src/components/Services.jsx
+++
@@ -1,7 +0,0 @@
-import classes from "../styles/services.module.css"
-
-const Services = () => {
-    return (<>Services</>);
-}
-
-export default Services;
